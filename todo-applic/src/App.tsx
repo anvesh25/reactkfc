2a03114e20032940aedb1bb0ab4c2c41c1ab5a20
slk <slk@WSLK1GFTR4P-09.SLKSOFT.COM>
2018-06-20 08:50:18 +0530

    change alignment

--- todo-applic/src/App.tsx
+++ todo-applic/src/App.tsx
@@ -15,30 +15,29 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"/>
-      </button>
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+          </button>
 
-      <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item active">
-              <Link className="nav-link" to="/add-todo">Add</Link>
-            </li>            
-          </ul>
-          <TodoCountContainer />
-        </div>
-    </nav>
+          <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/add-todo">Add</Link>
+                </li>            
+              </ul>
+              <TodoCountContainer />
+          </div>
+      </nav>
 
-    <main role="main" className="container">
-
-      <h1>Todo List</h1>
-      <Route exact ={true} path="/" component={HomeContainer} />
-      <Route path=".add-todo" component={AddTodoContainer}/>
-    </main>
+      <main role="main" className="container">
+        <h1>Todo List</h1>
+        <Route exact ={true} path="/" component={HomeContainer} />
+        <Route path=".add-todo" component={AddTodoContainer}/>
+      </main>
       </React.Fragment>
     );
   }
